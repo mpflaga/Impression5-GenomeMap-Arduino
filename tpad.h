3ab80371213b815d69d90f51788c5b6396b91aed
Michael P. Flaga <michael@flaga.net>
2022-01-21 14:47:58 -0500

    Changed All tpad tthresh to 7 and rthresh to 4. 5/2 was too sensitive.

--- tpad.h
+++ tpad.h
@@ -30,26 +30,26 @@ typedef struct {
 const mprs groups[] PROGMEM = {
   (mprs) {
     0x5A,
-    { 5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5}, //tthresh
-    { 2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2}, //rthresh
-  //{        1,       2, 3, 4, 5,     6,          7,    8,     9,    10,    11,              12}, //identifier
+    { 7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7}, //tthresh
+    { 4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4}, //rthresh
+  //{        1,       4, 3, 4, 7,     6,          7,    8,     9,     7,    11,              12}, //identifier
     {AUSTRALIA, SE_ASIA, 0, 0, 0, INDIA, KAZAKHSTAN, ASIA, KOREA, JAPAN, CHINA, PACIFIC_ISLANDS}, //identifier
   },
   { 0x5B,
-    { 5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5}, //tthresh
-    { 2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2}, //rthresh
-  //{13,   14,   15,   16,   17,   18,   19,   20,   21,   22,   23,   24}, //identifier
+    { 7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7}, //tthresh
+    { 4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4}, //rthresh
+  //{13,   14,   15,   16,   17,   18,   19,   40,   41,   42,   43,   44}, //identifier
     {TURKEY, 0, 0, MADAGASCAR, EAST_AFRICA, AFRICA, WEST_AFRICA, MALI, 0, SPAIN, BRITISH_ISLES, EUROPE}, //identifier
   },
   { 0x5C,
-    { 5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5}, //tthresh
-    { 2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2}, //rthresh
-  //{25,   26,   27,   28,   29,   30,   31,   32,   33,   34,   35,   36}, //identifier
+    { 7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7}, //tthresh
+    { 4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4}, //rthresh
+  //{25,   46,   47,   48,   49,   30,   31,   32,   33,   34,   35,   36}, //identifier
     {BOLIVIA_PERU, W_SOUTH_AMERICA, PANAMA, CARIBBEAN, 0, CANADA, NEW_ENGLAND, 0, 0, BRAZIL, 0, CHILE}, //identifier
   },
   { 0x5D,
-    { 5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5,    5}, //tthresh
-    { 2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2}, //rthresh
+    { 7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7,    7}, //tthresh
+    { 4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4,    4}, //rthresh
   //{37,   38,   39,   40,   41,   42,   43,   44,   45,   46,   47,   48}, //identifier
     {0, 0, 0, 0, CALIFORNIA, ARIZONA_NEW_MEXICO, E_NORTH_AMERICA, 0, MEXICO, 0, SOUTH_CAROLINA, 0}, //identifier
   }
